c439d8ed7928aff3aea19a5330a03b5f7b04d232
pamidev <pamidev@duck.com>
2023-02-21 23:08:20 +0100

    Edited post_list.html file and views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
