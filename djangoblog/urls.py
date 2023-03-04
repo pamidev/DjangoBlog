93abba5ed37071f7bc68f9fed07af0edbfd71355
pamidev <pamidev@duck.com>
2023-03-04 17:31:42 +0100

    Added new urls

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -17,14 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import Image, ImageDisplay
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('image/', Image.as_view(), name='image'),
-    path('image/<int:pk>/', ImageDisplay.as_view(), name="image_display"),
 ]
 
 handler404 = 'blog.views.error_404_view'
